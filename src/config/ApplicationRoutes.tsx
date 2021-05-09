0fc0c674a1b8f26e9909f24da9f84a31aaae4036
Schubert David Rodriguez <schubert.david.r@gmail.com>
2021-05-09 01:35:11 -0500

    adds routes to application in order to be navigated

--- src/config/ApplicationRoutes.tsx
+++ src/config/ApplicationRoutes.tsx
@@ -1,15 +1,20 @@
 import { Redirect, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
-import React from 'react';
+import React, { Profiler } from 'react';
 import { AuthRoutes } from '../views/Auth';
 import { DashboardRoutes } from '../views/Dashboard';
+import { ProfileRoutes } from '../views/Profile';
+import { InvoiceRoutes } from '../views/Invoice';
+
 
 import PrivateRoute from '../PrivateRoute';
 
 const ApplicationRoutes = () => {
-       return (
+    return (
         <Router>
             <Switch>
                 <PrivateRoute exact path="/dashboard" component={DashboardRoutes} />
+                <PrivateRoute exact path="/profile" component={ProfileRoutes} />
+                <PrivateRoute exact path="/invoice" component={InvoiceRoutes} />
                 <Route path="/auth" component={AuthRoutes} />
                 <Redirect to="/auth" from="/" />
             </Switch>
